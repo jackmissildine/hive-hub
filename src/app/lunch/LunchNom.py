8ca6a44835e54b2df42b2587e0a49d890189cba7
SweatyBurger <bmw626262@live.com>
2018-06-28 11:16:30 -0500

    Make LunchNom more pythonic

--- src/app/lunch/LunchNom.py
+++ src/app/lunch/LunchNom.py
@@ -4,25 +4,7 @@ class LunchNom(object):
     restaurant = ""
     image_url = ""
 
-    def __init__(self, is_seconded, restaurant, image_url):
-        self.is_seconded = is_seconded
+    def __init__(self, restaurant, image_url, is_seconded):
         self.restaurant = restaurant
         self.image_url = image_url
-
-    def get_restaurant(self):
-        return self.restaurant
-
-    def get_image(self):
-        return self.image_url
-
-    def get_is_seconded(self):
-        return self.is_seconded
-
-    def set_restaurant(self, new_restaurant):
-        self.restaurant = new_restaurant
-
-    def set_image(self, new_image):
-        self.image_url = new_image
-
-    def set_is_seconded(self, is_seconded):
         self.is_seconded = is_seconded
