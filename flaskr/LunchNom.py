de525b149256a6aad6eecf0587ecded8b6c7e3d2
SweatyBurger <bmw626262@live.com>
2018-06-28 12:51:37 -0500

    Minor spelling changes

--- flaskr/LunchNom.py
+++ flaskr/LunchNom.py
@@ -1,10 +1,8 @@
 class LunchNom(object):
 
-    is_seconded = False
-    restaurant = ""
-    image_url = ""
-
-    def __init__(self, restaurant, image_url, is_seconded):
+    def __init__(self, restaurant=None, image_url=None, is_seconded=False):
         self.restaurant = restaurant
         self.image_url = image_url
         self.is_seconded = is_seconded
+
+    def
