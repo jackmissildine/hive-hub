97dcc913b2f7f9508d6c331dbe5fe4eda313f2ee
Chris <christopher.hershey719@gmial.com>
2018-06-28 14:05:37 -0500

    Added nomination db table

--- backend/app/LunchNom.py
+++
@@ -1,9 +0,0 @@
-class LunchNom(object):
-
-    def __init__(self, restaurant=None, image_url=None, is_seconded=False):
-        self.restaurant = restaurant
-        self.image_url = image_url
-        self.is_seconded = is_seconded
-
-    def second(self):
-        self.is_seconded = True
